672c504dbe0f349e47e00901c7d4cc0592c3df43
Hwajiin <borderline0px@gmail.com>
2021-12-04 09:12:47 +0900

    ✨ Create mixins, constants and grid layouts for entire styles

---
+++ src/layouts/grids/col.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import responsive from "../../base/constants/responsive";
+
+export interface ColProps {
+  sm?: number;
+  md?: number;
+  lg?: number;
+}
+
+const SCol = styled.div<ColProps>`
+  background-color: pink;
+  ${(props) =>
+    props.sm ? `width: ${(props.sm / responsive.columns.sm) * 100}%;` : null}
+
+  ${(props) =>
+    props.md
+      ? `${responsive.device["above-tablet"]} {
+                width: ${(props.md / responsive.columns.md) * 100}%; 
+            } `
+      : null}
+
+    ${(props) =>
+    props.lg
+      ? `${responsive.device["above-desktop"]} {
+        width: ${(responsive.gutter + responsive.unit.lg) * props.lg}px;
+    }`
+      : null}
+`;
+
+const Col: React.FC<ColProps> = (props) => {
+  const { sm, md, lg } = props;
+
+  return sm || md || lg ? (
+    <SCol sm={sm} md={md} lg={lg}>
+      {props.children}
+    </SCol>
+  ) : null;
+};
+
+export default Col;
