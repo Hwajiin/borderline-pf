fd1cd74be37f14041fffafdfeb9b8483f450be14
Hwajiin <borderline0px@gmail.com>
2021-12-04 22:47:38 +0900

    ✨ Create data file about category

---
+++ src/data/category.js
@@ -0,0 +1,24 @@
+const category = [
+  {
+    title: "home",
+    path: "/",
+  },
+  {
+    title: "about me",
+    path: "/aboutme",
+  },
+  {
+    title: "skills",
+    path: "/skills",
+  },
+  {
+    title: "projects",
+    path: "/projects",
+  },
+  {
+    title: "contact",
+    path: "/contact",
+  },
+];
+
+export default category;
