fe2e3c2a8995395225c0caf15053c36925edf3d0
Hwajiin <borderline0px@gmail.com>
2021-12-05 22:48:11 +0900

    📝 Add info file about contact page

---
+++ src/data/contact.js
@@ -0,0 +1,25 @@
+const contactInfo = {
+  title: "contact",
+  subtitle: "",
+
+  email: {
+    path: "borderline0px@gmail.com",
+    summary: "borderline0px@gmail.com",
+  },
+
+  github: {
+    path: "https://github.com/Hwajiin",
+    summary: "source code를 확인해보세요.",
+  },
+
+  velog: {
+    path: "https://velog.io/@borderline0px",
+    summary: "토이 프로젝트 제작 과정을 기록하고 있습니다.",
+  },
+  resume: {
+    path: "https://www.notion.so/GO-3761c24a3e824deeb77b05f7567de108",
+    summary: "Notion을 통해, 이력서를 정리하였습니다.",
+  },
+};
+
+export default contactInfo;
