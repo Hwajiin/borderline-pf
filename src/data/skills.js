5e32efbe7830433caf3f3bafa40af7f68650dbe9
Hwajiin <borderline0px@gmail.com>
2021-12-06 17:41:30 +0900

    📝 Add skills info for skills page

---
+++ src/data/skills.js
@@ -0,0 +1,38 @@
+import htmlCssJs from "../assets/logo/html-css-js.png";
+import typescript from "../assets/logo/typescript.png";
+import react from "../assets/logo/react.png";
+import sass from "../assets/logo/sass.png";
+
+const skillsInfo = {
+  title: "skills",
+  subtitle: "",
+  stack: [
+    {
+      name: "base",
+      lang: "HTML/CSS/JavaScript",
+      img: htmlCssJs,
+    },
+    {
+      name: "react",
+      lang: "React",
+      img: react,
+    },
+    {
+      name: "sass",
+      lang: "Sass",
+      img: sass,
+    },
+    {
+      name: "typescript",
+      lang: "TypeScript",
+      img: typescript,
+    },
+    {
+      name: "studying",
+      lang: "",
+      img: null,
+    },
+  ],
+};
+
+export default skillsInfo;
