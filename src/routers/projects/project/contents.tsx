c7ddaba9e24aa74ec1c52e97b09864c51999af52
Hwajiin <borderline0px@gmail.com>
2021-12-07 20:28:21 +0900

    ✨ Create contents page and set by index of projects page

---
+++ src/routers/projects/project/contents.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Main from "../../../layouts/main";
+import Stack from "../../../layouts/stack";
+
+const Contents = () => {
+  return (
+    <Main title="Projects">
+      <Stack></Stack>
+    </Main>
+  );
+};
+
+export default Contents;
---
+++ src/routers/projects/projects.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router";
+
+import Main from "../../layouts/main";
+
+const Projects = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default Projects;
