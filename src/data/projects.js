2b844ce65894cb8c04c64b60569fe7ae6ca90799
Hwajiin <borderline0px@gmail.com>
2021-12-07 20:18:02 +0900

    📝 Add data about projects in projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+const projectInfo = {
+  title: "",
+  subtitle: "",
+  projects: [
+    {
+      route: "/my-portfolio",
+      title: "My Portfolio",
+      subtitle: "",
+      description: "",
+      stack: ["React(CRA)", "TypeScript"],
+      thumbnail: "",
+      img: [],
+      deployment: "",
+      github: "",
+      velog: "",
+    },
+    {
+      route: "/covid19-info",
+      title: "Covid19 info page",
+      subtitle: "",
+      description: "",
+      stack: ["React(CRA)", "Styled-component", "TypeScript"],
+      thumbnail: "",
+      img: [],
+      deployment: "",
+      github: "",
+      velog: "",
+    },
+    {
+      route: "/mountain-landing",
+      title: "Mountain Landing Page",
+      subtitle: "",
+      description: "",
+      stack: ["HTML", "SCSS", "JavaScript"],
+      thumbnail: "",
+      img: [],
+      deployment: "",
+      github: "",
+      velog: "",
+    },
+  ],
+};
+
+export default projectInfo;
