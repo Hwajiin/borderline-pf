7709a32ac2adb5e844ff1dc001f11786bfc82b0e
Hwajiin <borderline0px@gmail.com>
2021-12-07 20:16:48 +0900

    📝 Add data about projects routes

--- src/data/category.js
+++ src/data/category.js
@@ -1,24 +1,45 @@
-const category = [
-  {
-    title: "home",
-    path: "/",
-  },
-  {
-    title: "about me",
-    path: "/aboutme",
-  },
-  {
-    title: "skills",
-    path: "/skills",
-  },
-  {
-    title: "projects",
-    path: "/projects",
-  },
-  {
-    title: "contact",
-    path: "/contact",
-  },
-];
+import projectInfo from "./projects";
+
+const category = {
+  base: [
+    {
+      title: "home",
+      path: "/",
+    },
+    {
+      title: "about me",
+      path: "/aboutme",
+    },
+    {
+      title: "skills",
+      path: "/skills",
+    },
+    {
+      title: "projects",
+      path: "/projects",
+    },
+    {
+      title: "contact",
+      path: "/contact",
+    },
+  ],
+  projects: [
+    {
+      title: "my portfolio",
+      path: "/projects/my-portfolio",
+      route: "my-portfolio",
+    },
+    {
+      title: "covid19 info page",
+      path: "/projects/covid19-info",
+      route: "covid19-info",
+    },
+    {
+      title: "mountain landing page",
+      path: "/projects/mountain-landing",
+      route: "mountain-landing",
+    },
+  ],
+};
 
 export default category;
