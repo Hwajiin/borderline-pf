abfc09fad16fc365e95651f5bf43d923d1e59f86
Hwajiin <borderline0px@gmail.com>
2021-12-10 20:20:59 +0900

    🎨 Improve usability about tag

--- src/modules/tag.tsx
+++ src/modules/tag.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import colors from "../base/constants/colors";
+import fonts from "../base/constants/fonts";
 
 interface TagProps {
   word: string;
 }
 
 const STag = styled.span`
-  width: auto;
-  height: 40px;
+  display: inline-block;
   padding: 5px;
+  font-size: ${fonts.size.md};
   color: ${colors.variable["gray-dark"]};
   border: 2px solid ${colors.variable.gray};
   border-radius: 5px;
