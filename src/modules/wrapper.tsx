1aa0b6476b3374427dbef3e4c0367940b3b70126
Hwajiin <borderline0px@gmail.com>
2021-12-11 20:02:24 +0900

    🎨 Improve the structure by adding flex, padding props

--- src/modules/wrapper.tsx
+++ src/modules/wrapper.tsx
@@ -3,17 +3,32 @@ import styled from "styled-components";
 import responsive from "../base/constants/responsive";
 import flexbox from "../base/mixins/flexbox";
 
-const SWrapper = styled.div`
+interface WrapperProps {
+  flex?: boolean;
+  padding?: number;
+}
+
+const SWrapper = styled.div<WrapperProps>`
   width: 100%;
   height: 100%;
+  padding: ${({ padding }) => `0 ${padding}px`};
 
   ${responsive.device["above-tablet"]} {
-    ${flexbox()}
+    ${({ flex }) => flex && flexbox("space-around")};
+    padding: ${({ padding }) => `${padding}px 0`};
   }
 `;
 
-const Wrapper: React.FC = ({ children }) => {
-  return <SWrapper>{children}</SWrapper>;
+const Wrapper: React.FC<WrapperProps> = ({
+  flex = false,
+  padding = 0,
+  children,
+}) => {
+  return (
+    <SWrapper flex={flex} padding={padding}>
+      {children}
+    </SWrapper>
+  );
 };
 
 export default Wrapper;
