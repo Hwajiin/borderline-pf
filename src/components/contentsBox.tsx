d0311104c9e7a835577220846a17fe1bfbce79a9
Hwajiin <borderline0px@gmail.com>
2021-12-11 19:58:53 +0900

    💄 Complete the overall style of ContentsBox component

--- src/components/contentsBox.tsx
+++ src/components/contentsBox.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import colors from "../base/constants/colors";
+import fonts from "../base/constants/fonts";
 import responsive from "../base/constants/responsive";
-import TitleBox from "../modules/titleBox";
+import whitespace from "../base/constants/whitespace";
+import flexbox from "../base/mixins/flexbox";
+
+type ProjectsType = {
+  title: string;
+  summary: string;
+};
 
 interface ContentsBoxProps {
   size: number;
+  projects: ProjectsType[];
 }
 
 const SContentsBox = styled.section<ContentsBoxProps>`
@@ -30,10 +39,70 @@ const SContentsBox = styled.section<ContentsBoxProps>`
   }
 `;
 
-const ContentsBox: React.FC<ContentsBoxProps> = ({ size }) => {
+const Header = styled.header`
+  width: 100%;
+  padding: ${whitespace.base.padding.sm}px 0;
+  margin-bottom: 50px;
+  ${flexbox()}
+`;
+
+const Title = styled.h2``;
+
+const List = styled.ul`
+  padding: 0 20px;
+
+  ${responsive.device["above-tablet"]} {
+    padding: 0 40px;
+  }
+`;
+
+const Item = styled.li`
+  ${flexbox("flex-start", "flex-start")}
+
+  &:not(:last-child) {
+    margin-bottom: 40px;
+  }
+`;
+
+const Number = styled.span`
+  margin-right: 20px;
+  font-size: 2em;
+  line-height: 0.8em;
+  color: ${colors.variable["gray-dark"]};
+
+  ${responsive.device["above-tablet"]} 
+    font-size: 3em;
+  }
+`;
+
+const ItemTitle = styled.h3`
+  font-size: 16px;
+  text-transform: capitalize;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: ${fonts.size.sm};
+`;
+
+const ContentsBox: React.FC<ContentsBoxProps> = ({ size, projects }) => {
   return (
-    <SContentsBox size={size}>
-      <TitleBox title="contents" />
+    <SContentsBox size={size} projects={projects}>
+      <Header>
+        <Title>Contents</Title>
+      </Header>
+
+      <List>
+        {projects.map((project, index) => (
+          <Item key={index}>
+            <Number>{index + 1 < 10 ? `0${index + 1}` : index + 1}</Number>
+            <div>
+              <ItemTitle>{project.title}</ItemTitle>
+              <Description>{project.summary}</Description>
+            </div>
+          </Item>
+        ))}
+      </List>
     </SContentsBox>
   );
 };
