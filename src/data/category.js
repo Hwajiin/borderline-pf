75b942c7bb0e9ef4b9847c056f27f346ee5556bd
Hwajiin <borderline0px@gmail.com>
2021-12-11 19:54:12 +0900

    📝 Add summary data for each project

--- src/data/category.js
+++ src/data/category.js
@@ -1,4 +1,4 @@
-import projectInfo from "./projects";
+import projectInfo from "./projects.ts";
 
 const category = {
   base: [
@@ -26,16 +26,19 @@ const category = {
   projects: [
     {
       title: "my portfolio",
+      summary: "CRA와 TypeScript를 사용하여 만든 나만의 웹 사이트",
       path: "/projects/my-portfolio",
       route: "my-portfolio",
     },
     {
       title: "covid19 info page",
+      summary: "코로나19 Open API를 활용하여 만든 데이터 조회 사이트",
       path: "/projects/covid19-info",
       route: "covid19-info",
     },
     {
       title: "mountain landing page",
+      summary: "웹 접근성과 성능을 고려하여 만든 랜딩 페이지",
       path: "/projects/mountain-landing",
       route: "mountain-landing",
     },
