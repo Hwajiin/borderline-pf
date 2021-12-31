f561516a69550fc945664a3d3670d1d79baa648b
Hwajiin <borderline0px@gmail.com>
2021-12-31 18:13:09 +0900

    ♿️ Handling unavailable links to increase accessibility

--- src/components/metaDataBox.tsx
+++ src/components/metaDataBox.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import responsive from "../base/constants/responsive";
 import whitespace from "../base/constants/whitespace";
-import Border from "../modules/border";
 import TitleBox from "../modules/titleBox";
 import StackList from "./stackList";
 import { ProjectType } from "../data/projects";
@@ -17,6 +16,10 @@ interface MetaDataBoxProps {
 
 type MetaDataBoxStyleProps = Pick<MetaDataBoxProps, "size">;
 
+type LinkType = {
+  isHref?: boolean;
+};
+
 const Section = styled.section<MetaDataBoxStyleProps>`
   width: 100%;
   padding: ${whitespace.base.padding.sm}px;
@@ -25,7 +28,15 @@ const Section = styled.section<MetaDataBoxStyleProps>`
     width: ${({ size }) => size}%;
     padding: ${whitespace.base.padding.lg}px;
   }
+
+  ${responsive.device["above-desktop"]} {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 `;
+
+const Data = styled.div``;
 
 const Description = styled.p`
   margin: 40px 0;
@@ -36,9 +47,12 @@ const Description = styled.p`
 const LinkList = styled.ul`
   width: 100%;
   margin-top: 10px;
+  border-top: 2px solid ${colors.variable.gray};
+  padding-top: 10px;
 
   ${responsive.device["above-tablet"]} {
     display: flex;
+    flex-wrap: wrap;
   }
 `;
 
@@ -49,13 +63,12 @@ const Item = styled.li`
 
   ${responsive.device["above-tablet"]} {
     &:not(:last-child) {
-      margin-bottom: 0px;
       margin-right: 10px;
     }
   }
 `;
 
-const SLink = styled.a`
+const SLink = styled.a<LinkType>`
   ${flexbox()}
   font-size: ${fonts.size.md};
   height: 48px;
@@ -71,36 +84,56 @@ const SLink = styled.a`
   ${responsive.device["above-tablet"]} {
     width: 200px;
   }
+
+  ${({ isHref }) =>
+    !isHref
+      ? `
+    pointer-events: none;
+    opacity: 0.5;
+  `
+      : ""}
 `;
 
 const MetaDataBox: React.FC<MetaDataBoxProps> = ({ size, projectData }) => {
   return (
     <Section size={size}>
-      <TitleBox
-        title={projectData.title}
-        subtitle={projectData.subtitle}
-        center={false}
-      />
+      <Data>
+        <TitleBox
+          title={projectData.title}
+          subtitle={projectData.subtitle}
+          center={false}
+        />
 
-      <StackList stackList={projectData.stack} />
+        <StackList stackList={projectData.stack} />
 
-      <Description>{projectData.description}</Description>
-
-      <Border />
+        <Description>{projectData.description}</Description>
+      </Data>
 
       <LinkList>
         <Item>
-          <SLink href={projectData.deployment} target="_blank">
+          <SLink
+            href={projectData.deployment}
+            target="_blank"
+            isHref={projectData.deployment === "" ? false : true}
+          >
             Deployment
           </SLink>
         </Item>
         <Item>
-          <SLink href={projectData.github} target="_blank">
+          <SLink
+            href={projectData.github}
+            target="_blank"
+            isHref={projectData.github === "" ? false : true}
+          >
             Github
           </SLink>
         </Item>
         <Item>
-          <SLink href={projectData.velog} target="_blank">
+          <SLink
+            href={projectData.velog}
+            target="_blank"
+            isHref={projectData.velog === "" ? false : true}
+          >
             Velog
           </SLink>
         </Item>
