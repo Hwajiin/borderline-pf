d5f015351a3d0604cf92fac92608648b194efdb7
Hwajiin <borderline0px@gmail.com>
2022-02-16 20:15:45 +0900

    ♻️ lazyLoading 함수 구현하여 중복 코드 개선

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,21 @@ import Spinner from "./components/spinner";
 import imagePath from "./data/image_path";
 import useImage from "./hooks/useImage";
 import Navbar from "./layouts/navbar";
-import Home from "./routers/home/home";
+import HomePage from "./routers/home/home";
 import Projects from "./routers/projects/projects";
 
-const AboutMe = lazy(() => import("./routers/aboutMe/aboutMe"));
-const Skills = lazy(() => import("./routers/skills/skills"));
-const Contact = lazy(() => import("./routers/contact/contact"));
+const lazyLoading = (path: string) => {
+  return lazy(() => import(`./routers/${path}`));
+};
 
-const Contents = lazy(() => import("./routers/projects/project/contents"));
-const MyPortfolio = lazy(
-  () => import("./routers/projects/project/myPortfolio")
-);
-const CovidInfo = lazy(() => import("./routers/projects/project/covidInfo"));
-const MountainLanding = lazy(
-  () => import("./routers/projects/project/mountainLanding")
-);
+const AboutMePage = lazyLoading("aboutMe/aboutMe");
+const SkillsPage = lazyLoading("skills/skills");
+const ContactPage = lazyLoading("contact/contact");
+
+const ProjectsContentsPage = lazyLoading("projects/project/contents");
+const MyPortfolioPage = lazyLoading("projects/project/myPortfolio");
+const CovidInfoPage = lazyLoading("projects/project/covidInfo");
+const MountainLandingPage = lazyLoading("projects/project/mountainLanding");
 
 function App() {
   useImage([imagePath.home.bg.sm, imagePath.home.bg.md, imagePath.home.bg.lg]);
@@ -28,22 +28,20 @@ function App() {
       <Navbar />
       <Suspense fallback={<Spinner />}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="aboutme" element={<AboutMe />} />
-          <Route path="skills" element={<Skills />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="aboutme" element={<AboutMePage />} />
+          <Route path="skills" element={<SkillsPage />} />
           <Route path="projects" element={<Projects />}>
-            <Route index element={<Contents />} />
-            <Route path="my-portfolio" element={<MyPortfolio />} />
-            <Route path="covid19-info" element={<CovidInfo />} />
-            <Route path="mountain-landing" element={<MountainLanding />} />
-            <Route path="*" element={<Contents />} />
+            <Route index element={<ProjectsContentsPage />} />
+            <Route path="my-portfolio" element={<MyPortfolioPage />} />
+            <Route path="covid19-info" element={<CovidInfoPage />} />
+            <Route path="mountain-landing" element={<MountainLandingPage />} />
+            <Route path="*" element={<ProjectsContentsPage />} />
           </Route>
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<Home />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="*" element={<HomePage />} />
         </Routes>
       </Suspense>
-
-      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
