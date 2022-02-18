58d550ebc3789af790e82c1eac7bd365f535823c
Hwajiin <borderline0px@gmail.com>
2022-02-18 19:12:52 +0900

    🏷️ ContactInfoType을 정의, object 구조 개선

---
+++ src/data/contact.ts
@@ -0,0 +1,37 @@
+interface ContactInfoType {
+  title: string;
+  subtitle: string | null;
+  means: {
+    [key: string]: {
+      [key: string]: string;
+    };
+  };
+}
+
+const contactInfo: ContactInfoType = {
+  title: "contact",
+  subtitle: "",
+
+  means: {
+    email: {
+      path: "borderline0px@gmail.com",
+      summary: "borderline0px@gmail.com",
+    },
+
+    github: {
+      path: "https://github.com/Hwajiin",
+      summary: "source code를 확인해보세요.",
+    },
+
+    velog: {
+      path: "https://velog.io/@borderline0px",
+      summary: "프로젝트 제작 과정을 기록하고 있습니다.",
+    },
+    resume: {
+      path: "https://glimmer-pencil-ab4.notion.site/GO-3761c24a3e824deeb77b05f7567de108",
+      summary: "Notion을 통해, 이력서를 관리하고 있습니다.",
+    },
+  },
+};
+
+export default contactInfo;
